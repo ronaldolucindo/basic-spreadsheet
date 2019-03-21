2369173c5f977fea45fbaa34fc1f87f5706b5852
Ronaldo Lucindo <13293540+ronaldolucindo@users.noreply.github.com>
2019-03-20 23:01:41 -0300

    Insert add column modal

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { StoreContext } from "../../store";
+import { Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
 
 function Header() {
+  const [state, dispatch] = useContext(StoreContext);
+  const [modal, setModal] = useState(false);
+
+  const toggleModal = e => {
+    const currentState = modal;
+    setModal(!currentState);
+  };
+
   return (
     <nav className="navbar navbar-light bg-light sticky-top">
       <a className="navbar-brand" href="#">
@@ -15,13 +25,69 @@ function Header() {
         </li>
       </ul>
       <form className="form-inline">
-        <button className="btn btn-outline-primary my-2 mx-1 my-sm-0 right">
+        {/* () => dispatch({type: 'addColumn'}) */}
+        <button
+          onClick={toggleModal}
+          className="btn btn-outline-primary my-2 mx-1 my-sm-0 right"
+        >
           Add Column
         </button>
         <button className="btn btn-outline-success my-2 mx-1 my-sm-0 right">
           Save
         </button>
       </form>
+      <Modal isOpen={modal} toggle={toggleModal}>
+        <ModalHeader toggle={toggleModal}>Add new column</ModalHeader>
+        <form>
+          <ModalBody>
+            <div className="form-group">
+              <label for="columnName">Name</label>
+              <input
+                type="text"
+                className="form-control"
+                id="columnName"
+                placeholder="insert column name"
+                required
+              />
+            </div>
+            <div class="form-group">
+              <label for="columnType">Select column type</label>
+              <select class="form-control" id="columnType" required>
+                <option />
+                <option>date</option>
+                <option>number</option>
+                <option>text</option>
+              </select>
+            </div>
+            <div class="form-check">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                value=""
+                id="isRequired"
+              />
+              <label className="form-check-label" for="isRequired">
+                Required
+              </label>
+            </div>
+          </ModalBody>
+          <ModalFooter>
+            <button
+              // onClick={toggleModal}
+              className="btn btn-outline-success my-2 mx-1 my-sm-0"
+              type="submit"
+            >
+              OK
+            </button>
+            <button
+              onClick={toggleModal}
+              className="btn btn-outline-danger my-2 mx-1 my-sm-0"
+            >
+              Cancel
+            </button>
+          </ModalFooter>
+        </form>
+      </Modal>
     </nav>
   );
 }
